0e999eb4fdc9d960e1f7cf662afde94b10266faf
patrick.bryant1 <>
2021-03-04 08:57:40 +0100

    trying new

--- model/transformer.py
+++ model/transformer.py
@@ -20,7 +20,7 @@ from categorical_focal_loss import SparseCategoricalFocalLoss
 #visualization
 from tensorflow.keras.callbacks import TensorBoard
 
-from multi_head_attention import MultiHeadAttention
+from multi_head_attention import MultiHeadSelfAttention
 #from lr_finder import LRFinder
 
 
@@ -46,7 +46,7 @@ parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'P
 class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
         super(TransformerBlock, self).__init__()
-        self.att = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.att = MultiHeadSelfAttention(embed_dim,num_heads)
         self.ffn = keras.Sequential(
             [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
         )
@@ -56,7 +56,7 @@ class TransformerBlock(layers.Layer):
         self.dropout2 = layers.Dropout(rate)
 
     def call(self, inputs, training):
-        attn_output = self.att(inputs, inputs)
+        attn_output = self.att(inputs)
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layernorm1(inputs + attn_output)
         ffn_output = self.ffn(out1)
