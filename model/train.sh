a1f6c033d3e151bacd4b624eb7400c4d2e5fe9a8
Patrick Bryant <pbryant@b-an01.hpc2n.umu.se>
2021-03-04 08:58:04 +0100

    check

--- model/train.sh
+++ model/train.sh
@@ -6,6 +6,6 @@ for PARAM_COMBO in {1..243}
 do
   for TEST_PARTITION in {0..4}
     do
-    ./transformer.py --train_data $TRAIN_DATA --test_partition $TEST_PARTITION --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --outdir $OUTDIR
+    python3 transformer.py --train_data $TRAIN_DATA --test_partition $TEST_PARTITION --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --outdir $OUTDIR
     done
 done
