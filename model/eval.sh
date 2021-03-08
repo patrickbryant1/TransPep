51e2affb309db06d4aea76ead9f2767759e7e4d5
patrick.bryant1 <>
2021-03-08 08:39:07 +0100

    fetching valid results over all test partitions

--- model/eval.sh
+++ model/eval.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+#Evaluate the loss
 DATADIR=../data/
 RESULTSDIR=../results/
 VARIABLE_PARAMS=./param_combos.csv
-./eval_loss.py --resultsdir $RESULTSDIR --datadir $DATADIR --variable_params $VARIABLE_PARAMS
+#./eval_loss.py --resultsdir $RESULTSDIR --datadir $DATADIR --variable_params $VARIABLE_PARAMS
+
+#Evaluate the validation
+RESULTSDIR=../results/
+./eval_valid.py --resultsdir $RESULTSDIR
---
+++ model/eval_valid.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import sys
+import numpy as np
+import pandas as pd
+import glob
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pdb
+#Arguments for argparse module:
+parser = argparse.ArgumentParser(description = '''Evaluate the validation of the trained models.''')
+parser.add_argument('--resultsdir', nargs=1, type= str, default=sys.stdin, help = 'Path to resultsdir.')
+
+
+
+###########FUNTIONS###########
+def average_validation(valid_df):
+    '''Average the MCC, Recall and Precision
+    '''
+    type_conversion = {'SP':'Sec/SPI','LIPO':'Sec/SPII','TAT':'Tat/SPI'}
+
+    Kingdoms = []
+    Types = []
+    av_MCC = []
+    std_MCC = []
+    av_prec = []
+    std_prec = []
+    av_recall = []
+    std_recall = []
+    for kingdom in ['ARCHAEA', 'NEGATIVE', 'POSITIVE', 'EUKARYA']:
+        sel = valid_df[valid_df.Kingdom==kingdom]
+        for type in ['SP', 'LIPO', 'TAT']:
+            Kingdoms.append(kingdom)
+            Types.append(type_conversion[type])
+            sel_type = sel[sel.Type==type]
+            av_MCC.append(np.average(sel_type['MCC']))
+            std_MCC.append(np.std(sel_type['MCC']))
+            av_prec.append(np.average(sel_type['Precision']))
+            std_prec.append(np.std(sel_type['Precision']))
+            av_recall.append(np.average(sel_type['Recall']))
+            std_recall.append(np.std(sel_type['Recall']))
+
+    av_std_df = pd.DataFrame()
+    av_std_df['Kingdom']=Kingdoms
+    av_std_df['Type']=Types
+    av_std_df['MCC average']=av_MCC
+    av_std_df['MCC std'] = std_MCC
+    av_std_df['Precision average'] = av_prec
+    av_std_df['Precision std'] = std_prec
+    av_std_df['Recall average'] = av_recall
+    av_std_df['Recall std'] = std_recall
+    pdb.set_trace()
+
+###########MAIN###########
+args = parser.parse_args()
+resultsdir = args.resultsdir[0]
+
+eval_dfs = glob.glob(resultsdir+'eval_df*.csv')
+
+valid_df = []
+for name in eval_dfs:
+    df = pd.read_csv(name)
+    test_partition = int(name.split('/')[-1][-5])
+    df['test_partition']=test_partition
+    valid_df.append(df)
+valid_df = pd.concat(valid_df)
+
+#Get average scores
+average_validation(valid_df)
