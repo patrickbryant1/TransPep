2e4341fa5e8551447950acbeb39386a0086c0b64
patrick.bryant1 <>
2021-03-09 10:42:45 +0100

    adding bench set

--- model/test/nested_test.py
+++ model/test/nested_test.py
@@ -94,18 +94,20 @@ def load_model(json_file, weights):
     return model
 
 
-def process_bench_set():
-    bench_meta, bench_seqs, bench_annotations = parse_and_format(args.train_data[0])
+def process_bench_set(bench_set,datadir):
+    bench_meta, bench_seqs, bench_annotations = parse_and_format(bench_set)
     #Save
-    train_meta.to_csv(datadir+'train_meta.csv')
-    np.save(datadir+'seqs.npy',train_seqs)
-    np.save(datadir+'annotations.npy',train_annotations)
+    bench_meta.to_csv(datadir+'bench_meta.csv')
+    np.save(datadir+'bench_seqs.npy',bench_seqs)
+    np.save(datadir+'bench_annotations.npy',bench_annotations)
 
+    return None
 
 def get_data(datadir, test_partition):
     '''Get the test data
     '''
 
+    #Get the test data
     train_meta = pd.read_csv(datadir+'train_meta.csv')
     train_seqs = np.load(datadir+'seqs.npy',allow_pickle=True)
     train_annotations = np.load(datadir+'annotations.npy',allow_pickle=True)
@@ -121,13 +123,35 @@ def get_data(datadir, test_partition):
     #Run through all by taking as input
     test_i = train_meta[train_meta.Partition==test_partition].index
 
-    #valid
+    #test
     x_test_seqs = train_seqs[test_i]
     x_test_kingdoms = train_kingdoms[test_i]
     x_test = [x_test_seqs,x_test_kingdoms]
     y_test = [train_annotations[test_i],train_types[test_i]]
 
-    return x_test, y_test
+    #Get the bench data
+    bench_meta = pd.read_csv(datadir+'bench_meta.csv')
+    bench_seqs = np.load(datadir+'bench_seqs.npy',allow_pickle=True)
+    bench_annotations = np.load(datadir+'bench_annotations.npy',allow_pickle=True)
+
+
+    bench_kingdoms = bench_meta.Kingdom.values
+    bench_meta['Type'] = bench_meta['Type'].replace({'NO_SP':0,'SP':1,'TAT':2,'LIPO':3})
+    bench_types = bench_meta.Type.values
+    #Onehot conversion
+    bench_kingdoms = np.eye(4)[bench_kingdoms]
+
+    #Get data
+    #Run through all by taking as input
+    bench_i = bench_meta[bench_meta.Partition==test_partition].index
+
+    #bench
+    x_bench_seqs = bench_seqs[bench_i]
+    x_bench_kingdoms = bench_kingdoms[bench_i]
+    x_bench = [x_bench_seqs,x_bench_kingdoms]
+    y_bench = [bench_annotations[bench_i],bench_types[bench_i]]
+
+    return x_test, y_test, x_bench, y_bench
 
 def eval_type_cs(pred_annotations,pred_types,true_annotations,true_types,kingdom):
     '''Evaluate the capacity to predict the clevage site
@@ -223,61 +247,106 @@ def eval_type_cs(pred_annotations,pred_types,true_annotations,true_types,kingdom
 args = parser.parse_args()
 checkpointdir=args.checkpointdir[0]
 datadir = args.datadir[0]
+bench_set = args.bench_set[0]
 outdir = args.outdir[0]
+
+#Preprocess the bench set
+process_bench_set(bench_set,datadir)
 
 kingdom_conversion = {'ARCHAEA':0,'NEGATIVE':2,'POSITIVE':3,'EUKARYA':1}
 #Load and run model
-all_pred_annotations = []
-all_pred_types = []
-all_true_annotations = []
-all_true_types = []
-all_kingdoms = []
-
+#test
+test_all_pred_annotations = []
+test_all_pred_types = []
+test_all_true_annotations = []
+test_all_true_types = []
+test_all_kingdoms = []
+#bench
+bench_all_pred_annotations = []
+bench_all_pred_types = []
+bench_all_true_annotations = []
+bench_all_true_types = []
+bench_all_kingdoms = []
 #Get data for each test partition
 for test_partition in np.arange(5):
     #json file with model description
     json_file = checkpointdir+'TP'+str(test_partition)+'/model.json'
-    pred_annotations = []
-    pred_types = []
+    #test
+    test_pred_annotations = []
+    test_pred_types = []
+    #bench
+    bench_pred_annotations = []
+    bench_pred_types = []
     for valid_partition in  np.setdiff1d(np.arange(5),test_partition):
         #weights
         weights=glob.glob(checkpointdir+'TP'+str(test_partition)+'/vp'+str(valid_partition)+'/*.hdf5')
         #model
         model = load_model(json_file, weights[0])
         #Get data
-        x_test, y_test = get_data(datadir, test_partition)
-        pred = model.predict(x_test)
+        x_test, y_test, x_bench, y_bench = get_data(datadir, test_partition)
+        test_pred = model.predict(x_test)
+        test_bench = model.predict(x_bench)
         #Save
-        pred_annotations.append(pred[0][:,0,:,:])
-        pred_types.append(pred[1])
+        #test
+        test_pred_annotations.append(test_pred[0][:,0,:,:])
+        test_pred_types.append(test_pred[1])
+        #bench
+        bench_pred_annotations.append(bench_pred[0][:,0,:,:])
+        bench_pred_types.append(bench_pred[1])
 
     #Join all nested preds
+    #TEST
     #Annotations
-    pred_annotations = np.array(pred_annotations)
-    pred_annotations = np.average(pred_annotations,axis=0)
-    pred_annotations = np.argmax(pred_annotations,axis=2)
+    test_pred_annotations = np.array(test_pred_annotations)
+    test_pred_annotations = np.average(test_pred_annotations,axis=0)
+    test_pred_annotations = np.argmax(test_pred_annotations,axis=2)
     #Types
-    pred_types = np.array(pred_types)
-    pred_types = np.average(pred_types,axis=0)
-    pred_types = np.argmax(pred_types,axis=1)
-    true_annotations = y_test[0]
-    true_types = y_test[1]
-    kingdoms = np.argmax(x_test[1],axis=1)
+    test_pred_types = np.array(test_pred_types)
+    test_pred_types = np.average(test_pred_types,axis=0)
+    test_pred_types = np.argmax(test_pred_types,axis=1)
+    test_true_annotations = y_test[0]
+    test_true_types = y_test[1]
+    test_kingdoms = np.argmax(x_test[1],axis=1)
     #Save
-    all_pred_types.extend([*pred_types])
-    all_pred_annotations.extend([*pred_annotations])
-    all_true_types.extend([*true_types])
-    all_true_annotations.extend([*true_annotations])
-    all_kingdoms.extend([*kingdoms])
+    test_all_pred_types.extend([*test_pred_types])
+    test_all_pred_annotations.extend([*test_pred_annotations])
+    test_all_true_types.extend([*test_true_types])
+    test_all_true_annotations.extend([*test_true_annotations])
+    test_all_kingdoms.extend([*test_kingdoms])
 
+    #BENCH
+    #Annotations
+    bench_pred_annotations = np.array(bench_pred_annotations)
+    bench_pred_annotations = np.average(bench_pred_annotations,axis=0)
+    bench_pred_annotations = np.argmax(bench_pred_annotations,axis=2)
+    #Types
+    bench_pred_types = np.array(bench_pred_types)
+    bench_pred_types = np.average(bench_pred_types,axis=0)
+    bench_pred_types = np.argmax(bench_pred_types,axis=1)
+    bench_true_annotations = y_bench[0]
+    bench_true_types = y_bench[1]
+    bench_kingdoms = np.argmax(x_bench[1],axis=1)
+    #Save
+    bench_all_pred_types.extend([*bench_pred_types])
+    bench_all_pred_annotations.extend([*bench_pred_annotations])
+    bench_all_true_types.extend([*bench_true_types])
+    bench_all_true_annotations.extend([*bench_true_annotations])
+    bench_all_kingdoms.extend([*bench_kingdoms])
 
 #Array conversions
-all_pred_annotations = np.array(all_pred_annotations)
-all_pred_types = np.array(all_pred_types)
-all_true_annotations = np.array(all_true_annotations)
-all_true_types = np.array(all_true_types)
-all_kingdoms = np.array(all_kingdoms)
-
+#test
+test_all_pred_annotations = np.array(test_all_pred_annotations)
+test_all_pred_types = np.array(test_all_pred_types)
+test_all_true_annotations = np.array(test_all_true_annotations)
+test_all_true_types = np.array(test_all_true_types)
+test_all_kingdoms = np.array(test_all_kingdoms)
+#bench
+bench_all_pred_annotations = np.array(bench_all_pred_annotations)
+bench_all_pred_types = np.array(bench_all_pred_types)
+bench_all_true_annotations = np.array(bench_all_true_annotations)
+bench_all_true_types = np.array(bench_all_true_types)
+bench_all_kingdoms = np.array(bench_all_kingdoms)
+pdb.set_trace()
 #Evaluate per kingdom
 evaluated_kingdoms = []
 all_types = []
