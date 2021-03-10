1339ad569acd2281f2d3e4c1f11b76c2dc9f9c0f
patrick.bryant1 <>
2021-03-10 12:18:54 +0100

    getting attention

--- signal/model/analyze/analyze.sh
+++ signal/model/analyze/analyze.sh
@@ -3,11 +3,12 @@
 CHECKPOINTDIR=../../checkpoint/
 DATADIR=../../data/
 BENCH_SET=../../data/benchmark_set.fasta
-OUTDIR=/home/patrick/results/protein_translation/attentions/
+OUTDIR=/home/patrick/results/protein_translation/attention/
 
 TP=0
+mkdir $OUTDIR'/TP'$TP
 for VP in 1 2 3 4
 do
   mkdir $OUTDIR'/TP'$TP'/VP'$VP
-  python3 get_attention.py --checkpointdir $CHECKPOINTDIR --datadir $DATADIR --test_partition $TP --valid_partition $VP &> $OUTDIR'/TP'$TP'/VP'$VP'/activations.txt' 
+  python3 get_attention.py --checkpointdir $CHECKPOINTDIR --datadir $DATADIR --test_partition $TP --valid_partition $VP &> $OUTDIR'/TP'$TP'/VP'$VP'/activations.txt'
 done
