f55c61e81d085c3d0856863f961c53c19b5e1114
patrick.bryant1 <>
2021-03-23 20:25:31 +0100

    update

--- signal/model/analyze/plot_bench.py
+++ signal/model/analyze/plot_bench.py
@@ -22,14 +22,23 @@ parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'O
 def plot_scores(type_CSV,type,outdir):
     '''Plot the distributions
     '''
+    if type == 'Sec/SPI':
+        height=6/2.54
+        aspect=9/4.5
+    else:
+         height=6/(2.54)
+         aspect=4.5/4.5
+
     for score in ['MCC','Recall','Precision']:
-        fig,ax = plt.subplots(figsize=(12/2.54,4.5/2.54))
+
         sns.color_palette("Set2")
         sns.set_style("whitegrid")
-        g = sns.catplot(x='Method', y=score, hue='Kingdom', kind="bar", data=type_CSV, palette="Paired",legend_out=False)
+        fig,ax = plt.subplots(figsize=(180/2.54,6/2.54))
+        g = sns.catplot(x='Method', y=score, hue='Kingdom', kind="bar",
+        data=type_CSV, palette="Paired",legend=False, height=height, aspect=aspect)
         g.set_xticklabels(rotation=45)
         plt.ylim([0,1])
-        plt.title(type)
+        plt.title(type+' '+score)
         plt.tight_layout()
         plt.savefig(outdir+'_'.join(type.split('/'))+'_'+score+'.png',dpi=300)
 
@@ -42,6 +51,7 @@ args = parser.parse_args()
 benchcsv = pd.read_csv(args.benchcsv[0])
 outdir = args.outdir[0]
 matplotlib.rcParams.update({'font.size': 7})
+
 #Fix data
 benchcsv = benchcsv.replace('n.d.',0)
 for score in ['MCC','Recall','Precision']:
