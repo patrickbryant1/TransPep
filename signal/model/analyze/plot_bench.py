f2d8efae4805f4e5eb9df4a8aab68fe28ab049d3
patrick.bryant1 <>
2021-03-24 09:13:29 +0100

    montage

--- signal/model/analyze/plot_bench.py
+++ signal/model/analyze/plot_bench.py
@@ -19,15 +19,29 @@ parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'O
 
 
 ##########FUNCTIONS###########
+
+def export_legend(legend, filename="legend.png"):
+    fig2  = legend.figure
+    fig2.canvas.draw()
+    bbox  = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
+    fig2.savefig(filename, dpi=300, bbox_inches=bbox)
+
+
+
+
 def plot_scores(type_CSV,type,outdir):
     '''Plot the distributions
     '''
     if type == 'Sec/SPI':
         height=6/2.54
         aspect=9/4.5
+        palette = ['cornflowerblue','mediumblue','palegreen','seagreen']
     else:
-         height=6/(2.54)
+         height=6/2.54
          aspect=4.5/4.5
+         palette = ['cornflowerblue','palegreen','seagreen']
+
+
 
     for score in ['MCC','Recall','Precision']:
 
@@ -35,13 +49,18 @@ def plot_scores(type_CSV,type,outdir):
         sns.set_style("whitegrid")
         fig,ax = plt.subplots(figsize=(180/2.54,6/2.54))
         g = sns.catplot(x='Method', y=score, hue='Kingdom', kind="bar",
-        data=type_CSV, palette="Paired",legend=False, height=height, aspect=aspect)
+            data=type_CSV, palette=palette,legend=False, height=height, aspect=aspect)
+
         g.set_xticklabels(rotation=45)
         plt.ylim([0,1])
         plt.title(type+' '+score)
+        #if type =='Sec/SPI':
+            #legend = plt.legend()
+            #export_legend(legend,outdir+'legend.png')
+            #legend.remove()
         plt.tight_layout()
         plt.savefig(outdir+'_'.join(type.split('/'))+'_'+score+'.png',dpi=300)
-
+        plt.close()
 
 
 
@@ -56,6 +75,11 @@ matplotlib.rcParams.update({'font.size': 7})
 benchcsv = benchcsv.replace('n.d.',0)
 for score in ['MCC','Recall','Precision']:
     benchcsv[score] = np.array(benchcsv[score],dtype='int')/1000
+#Set colors
+benchcsv['color']='b'
 for type in benchcsv.Type.unique():
     type_CSV = benchcsv[benchcsv.Type==type]
     plot_scores(type_CSV,type,outdir)
+
+
+#Plot legend
