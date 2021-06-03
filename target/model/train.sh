e4aed4b6ac3f29dfb7331d730563d72756790c31
patrick.bryant1 <>
2021-06-03 07:35:52 +0200

    train best

--- target/model/train.sh
+++ target/model/train.sh
@@ -4,9 +4,25 @@ VARIABLE_PARAMS=./param_combos.csv
 CHECKPOINTDIR=../results/checkpoint/
 SAVE_MODEL=1
 CHECKPOINT=1
-NUM_EPOCHS=5
+NUM_EPOCHS=30
 OUTDIR=../results/
 
-PARAM_COMBO=1
+PARAM_COMBO=691
 TEST_PARTITION=0
-python3 ./transformer.py --datadir $DATADIR --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --checkpointdir $CHECKPOINTDIR'TP'$TEST_PARTITION'/' --checkpoint $CHECKPOINT --num_epochs $NUM_EPOCHS --outdir $OUTDIR
+python3 ./transformer.py --datadir $DATADIR --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --checkpointdir $CHECKPOINTDIR'/' --checkpoint $CHECKPOINT --num_epochs $NUM_EPOCHS --outdir $OUTDIR
+
+PARAM_COMBO=837
+TEST_PARTITION=1
+python3 ./transformer.py --datadir $DATADIR --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --checkpointdir $CHECKPOINTDIR'/' --checkpoint $CHECKPOINT --num_epochs $NUM_EPOCHS --outdir $OUTDIR
+
+PARAM_COMBO=638
+TEST_PARTITION=2
+python3 ./transformer.py --datadir $DATADIR --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --checkpointdir $CHECKPOINTDIR'/' --checkpoint $CHECKPOINT --num_epochs $NUM_EPOCHS --outdir $OUTDIR
+
+PARAM_COMBO=564
+TEST_PARTITION=3
+python3 ./transformer.py --datadir $DATADIR --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --checkpointdir $CHECKPOINTDIR'/' --checkpoint $CHECKPOINT --num_epochs $NUM_EPOCHS --outdir $OUTDIR
+
+PARAM_COMBO=845
+TEST_PARTITION=4
+python3 ./transformer.py --datadir $DATADIR --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --checkpointdir $CHECKPOINTDIR'/' --checkpoint $CHECKPOINT --num_epochs $NUM_EPOCHS --outdir $OUTDIR
