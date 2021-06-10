8f75d2cbd8460dadf931c24c1b9d449ca602ad8e
patrick.bryant1 <>
2021-06-10 11:08:59 +0200

    find lr

---
+++ target/model/find_lr.sh
@@ -0,0 +1,14 @@
+
+DATADIR=../data/
+VARIABLE_PARAMS=./param_combos.csv
+CHECKPOINTDIR=../results/checkpoint/
+CHECKPOINT=0
+NUM_EPOCHS=30
+FINDLR=1
+OUTDIR=../results/
+
+for PARAM_COMBO in {1..360}
+do
+  echo $PARAM_COMBO
+  python3 ./transformer.py --datadir $DATADIR --variable_params $VARIABLE_PARAMS --param_combo $PARAM_COMBO --checkpointdir $CHECKPOINTDIR'/' --checkpoint $CHECKPOINT --num_epochs $NUM_EPOCHS --find_lr $FINDLR --outdir $OUTDIR
+done
