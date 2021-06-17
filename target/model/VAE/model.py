d8788ee1477cc00b8ba3b18b85f7598c7c1b2dda
patrick.bryant1 <>
2021-06-17 07:56:15 +0200

    transformer VAE

--- target/model/VAE/model.py
+++ target/model/VAE/model.py
@@ -34,7 +34,7 @@ def create_model(maxlen, vocab_size, embed_dim,num_heads, encode_dim,num_layers,
     '''
 
     #Latent dim - dimension for sampling
-    latent_dim = int(encode_dim/8)
+    latent_dim = encode_dim
     #Encoder
     encoder_inp = layers.Input(shape=(maxlen)) #Input AA sequence
     ##Embeddings
@@ -43,50 +43,31 @@ def create_model(maxlen, vocab_size, embed_dim,num_heads, encode_dim,num_layers,
 
     #Encode
     x = embedding_layer(encoder_inp)
-    #x, enc_attn_weights = enc_attention(x,x,x) #Initial attention layer
-    #Maxpool
-    #x = tf.keras.layers.GlobalMaxPooling1D( data_format='channels_last')(x)
-    x = layers.Dense(encode_dim,activation='relu')(x)
-    x = layers.Dropout(0.1)(x)
-    x = layers.BatchNormalization()(x) #Bacth normalize, focus on segment
-    x = layers.Dense(int(encode_dim/2),activation='relu')(x)
-    x = layers.Dropout(0.1)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dense(int(encode_dim/4),activation='relu')(x)
-    x = layers.Dropout(0.1)(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Flatten()(x)
-    #Constrain to distribution
-    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
-    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
-    #z = z_mean + exp(z_log_sigma) * epsilon, where epsilon is a random normal tensor.
-    z = Sampling(name='z')([z_mean, z_log_var])
+    #Attention layer
+    for i in range(2):
+        x, enc_attn_weights = enc_attention(x,x,x)
+    #Flatten
+    x = layers.Reshape((maxlen*embed_dim,))(x) # (batch_size, seq_len, embed_dim)
+    z = layers.Dense(latent_dim, name="z")(x)
+    # #Constrain to distribution
+    # z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    # z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    # #z = z_mean + exp(z_log_sigma) * epsilon, where epsilon is a random normal tensor.
+    # z = Sampling(name='z')([z_mean, z_log_var])
     #model
-    encoder = keras.Model(encoder_inp, [z_mean, z_log_var, z], name="encoder")
+    encoder = keras.Model(encoder_inp, [z], name="encoder")
     print(encoder.summary())
 
     #Decoder
     latent_inp = keras.Input(shape=(latent_dim))
-
-    x = layers.Dense(int(encode_dim/4)*maxlen,activation='relu')(latent_inp)
-    x = layers.Reshape((maxlen,int(encode_dim/4)))(x)
-
-    x = layers.Dropout(0.1)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dense(int(encode_dim/2),activation='relu')(x)
-    x = layers.Dropout(0.1)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dense(encode_dim,activation='relu')(x)
-    x = layers.Dropout(0.1)(x)
-    x = layers.BatchNormalization()(x)
-
+    #Dense
+    x = layers.Dense(maxlen*embed_dim,activation='relu')(latent_inp)
+    #Reshape
+    x = layers.Reshape((maxlen,embed_dim))(x)
     #decoder attention
     dec_attention = MultiHeadSelfAttention(maxlen,num_heads)
-    #x, enc_attn_weights = dec_attention(x,x,x)
-    #Maxpool
-    #x = tf.keras.layers.GlobalMaxPooling1D( data_format='channels_last')(x)
-    #x_exp = tf.expand_dims(x,-1)
+    for i in range(2):
+        x, enc_attn_weights = dec_attention(x,x,x)
     #Final
     preds = layers.Dense((vocab_size), activation="softmax")(x) #Annotate
     #model
@@ -94,15 +75,15 @@ def create_model(maxlen, vocab_size, embed_dim,num_heads, encode_dim,num_layers,
     print(decoder.summary())
 
     #VAE
-    vae_outp = decoder(encoder(encoder_inp)[2]) #Inp z to decoder
+    vae_outp = decoder(encoder(encoder_inp)) #Inp z to decoder
     vae = keras.Model(encoder_inp, vae_outp, name='vae')
     #Loss
     reconstruction_loss = keras.losses.SparseCategoricalCrossentropy()(encoder_inp,vae_outp) #true,pred
     #kl loss
-    kl_loss = 1 + z_log_var - keras.backend.square(z_mean) - keras.backend.exp(z_log_var)
-    kl_loss = keras.backend.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    vae_loss = keras.backend.mean(reconstruction_loss + kl_loss)
+    # kl_loss = 1 + z_log_var - keras.backend.square(z_mean) - keras.backend.exp(z_log_var)
+    # kl_loss = keras.backend.sum(kl_loss, axis=-1)
+    # kl_loss *= -0.5
+    vae_loss = reconstruction_loss  #keras.backend.mean(reconstruction_loss + kl_loss)
 
     #Optimizer
     initial_learning_rate = 1e-1
