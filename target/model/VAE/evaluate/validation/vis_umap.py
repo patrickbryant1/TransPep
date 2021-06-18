2acd3662c47c556e94c6b9f0aa8ce15e1efc5575
patrick.bryant1 <>
2021-06-18 17:19:36 +0200

    plotting

--- target/model/VAE/evaluate/validation/vis_umap.py
+++ target/model/VAE/evaluate/validation/vis_umap.py
@@ -34,26 +34,73 @@ def vis_umap(us, us_seq, all_types, all_orgs):
     type_colors = {'NA':'grey','SP':'tab:blue','MT':'magenta','CH':'g','TH':'k'}
     types = [type_descr[i] for i in all_types]
     org_descr = {0:'No plant',1:'Plant'}
+    org_colors = {'No plant':'grey','Plant':'g'}
     orgs = [org_descr[i] for i in all_orgs]
     df = pd.DataFrame()
     df['u1']=us[:,0]
     df['u2']=us[:,1]
+    df['u1_seq']=us_seq[:,0]
+    df['u2_seq']=us_seq[:,1]
     df['type']=types
     df['org']=orgs
     #Plot
 
-    #Types
+    #Types enc
     fig,ax = plt.subplots(figsize=(12/2.54,12/2.54))
     for type in df.type.unique():
         sel = df[df.type==type]
         plt.scatter(sel['u1'],sel['u2'],s=2,color=type_colors[type],label=type,alpha=0.5)
     plt.legend()
+    plt.title('Embeddings')
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     plt.xlabel('u1')
     plt.ylabel('u2')
     plt.tight_layout()
     plt.savefig(outdir+'type_umap.png',dpi=300)
+
+    #Types seq
+    fig,ax = plt.subplots(figsize=(12/2.54,12/2.54))
+    for type in df.type.unique():
+        sel = df[df.type==type]
+        plt.scatter(sel['u1_seq'],sel['u2_seq'],s=2,color=type_colors[type],label=type,alpha=0.5)
+    plt.legend()
+    plt.title('Sequences')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.xlabel('u1')
+    plt.ylabel('u2')
+    plt.tight_layout()
+    plt.savefig(outdir+'type_umap_seq.png',dpi=300)
+
+    #Org enc
+    fig,ax = plt.subplots(figsize=(12/2.54,12/2.54))
+    for org in df.org.unique():
+        sel = df[df.org==org]
+        plt.scatter(sel['u1'],sel['u2'],s=2,color=org_colors[org],label=org,alpha=0.5)
+    plt.legend()
+    plt.title('Embeddings')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.xlabel('u1')
+    plt.ylabel('u2')
+    plt.tight_layout()
+    plt.savefig(outdir+'org_umap.png',dpi=300)
+
+    #Org enc
+    fig,ax = plt.subplots(figsize=(12/2.54,12/2.54))
+    for org in df.org.unique():
+        sel = df[df.org==org]
+        plt.scatter(sel['u1_seq'],sel['u2_seq'],s=2,color=org_colors[org],label=org,alpha=0.5)
+    plt.legend()
+    plt.title('Sequences')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.xlabel('u1')
+    plt.ylabel('u2')
+    plt.tight_layout()
+    plt.savefig(outdir+'org_umap_seq.png',dpi=300)
+
     pdb.set_trace()
 ######################MAIN######################
 args = parser.parse_args()
@@ -77,7 +124,7 @@ for i in [0]:
 
     try:
         us =np.load(datadir+'umap'+str(i)+'.npy',allow_pickle=True)
-        us =np.load(datadir+'umap_seq'+str(i)+'.npy',allow_pickle=True)
+        us_seq =np.load(datadir+'umap_seq'+str(i)+'.npy',allow_pickle=True)
     except:
         print('Mapping UMAP for encodings...')
         us = umap.UMAP().fit_transform(all_encodings_z)
